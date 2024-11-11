5936c1d633091a6758f4d702ed0a18afd2b7afed
Quannguyen2541 <quandz.5114@gmail.com>
2024-11-11 14:00:20 +0700

    bai 6 update

--- bai6_ChuyenSoThanhChu.cpp
+++ bai6_ChuyenSoThanhChu.cpp
@@ -18,6 +18,41 @@ int main() {
 	else if (num == 6) { cout << "Sau\n"; }
 	else if (num == 7) { cout << "Bay\n"; }
 	else if (num == 8) { cout << "Tam\n"; }
-	else if (num == 9) { cout << "Chin\n" << endl; }
-	return 0;
+	else if (num == 9) { cout << "Chin\n"; }
+	
+	switch (num)
+	{
+	case 0:
+		cout << "khong\n";
+		break;
+	case 1:
+		cout << "Mot\n";
+		break;
+	case 2:
+		cout << "hai\n";
+		break;
+	case 3:
+		cout << "ba\n";
+		break;
+	case 4:
+		cout << "bon\n";
+		break;
+	case 5:
+		cout << "nam\n";
+		break;
+	case 6:
+		cout << "sau\n";
+		break;
+	case 7:
+		cout << "bay\n";
+		break;
+	case 8:
+		cout << "tam\n";
+		break;
+	case 9:
+		cout << "chin\n";
+		break;
+		return 0;
+
+	}
 }
